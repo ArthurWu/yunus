aac3b565bcd3847e1b83aa53f3c42b8aa66c2060
Arthur  Wu <arthur.wu@quest.com>
2012-11-23 22:07:29 +0800

    update file

--- centre/admin.py
+++ centre/admin.py
@@ -1,5 +1,11 @@
+# encoding: utf-8
 from centre.models import *
 from django.contrib import admin
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ArticleAdmin(admin.ModelAdmin):
     fields = ('menu','title','title_english','summary','summary_english', 'body','body_english', 'image')
@@ -22,4 +28,33 @@ class HomePictureAdmin(admin.ModelAdmin):
 	list_editable = ('order',)
 
 admin.site.register(HomePicture, HomePictureAdmin)
-admin.site.register(Subscription)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+	actions = ['send_email']
+
+	def send_email(self, request, queryset):
+		opts = self.model._meta
+		app_label = opts.app_label
+		logger.debug('send email ...')
+		#send_mail(subject, message, from_email, recipient_list, fail_silently=False, auth_user=None, auth_password=None, connection=None)
+		if request.POST.get('post'):
+			subject = request.POST.get('title', '')
+			body = request.POST.get('body', '')
+			from_email = '175040128@qq.com'
+			recipient_list = [i.email for i in queryset]
+			attachment = request.FILES['attachment']
+			msg = EmailMessage(subject, body, from_email, recipient_list)
+			msg.attach('file.jpg', attachment)#, mimetype)
+			try:
+				msg.send()
+			except Exception:
+				return HttpResponse('邮件发送失败！')
+			return HttpResponse('邮件发送成功！')
+
+		title = u"发送邮件给订阅用户"
+
+		return render(request, 'send_email_confirmation.html', locals())
+		
+	send_email.short_description = u'发送邮件'
+
+admin.site.register(Subscription, SubscriptionAdmin)
